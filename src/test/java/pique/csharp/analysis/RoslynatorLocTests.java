1dcc2a25d3cdcd88ca7f344d7dcb3cb88a5e1b2a
david_rice <rice@s2corporation.com>
2020-12-15 23:21:25 -0700

    Add tests for RoslynatorLoc

--- src/test/java/pique/csharp/analysis/RoslynatorLocTests.java
+++ src/test/java/pique/csharp/analysis/RoslynatorLocTests.java
@@ -4,58 +4,56 @@ import org.apache.commons.io.FileUtils;
 import org.junit.Assert;
 import org.junit.Test;
 import pique.model.Diagnostic;
+import pique.model.Finding;
 
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Map;
 
-public class RoslynatorTests {
+public class RoslynatorLocTests {
 
     private final Path ROSLYN_ROOT = Paths.get("src/main/resources/Roslynator");
     private final Path MS_BUILD = Paths.get("C:/Program Files (x86)/Microsoft Visual " +
             "Studio/2019/Community/MSBuild/Current/Bin");
     private final Path TARGET = Paths.get("src/test/resources/projects/TestNetFramework");
-    private final Path ROSLYN_OUTPUT = Paths.get("out/roslynator_output.xml");
+    private final Path ROSLYN_OUTPUT = Paths.get("out/roslynator_loc_output.xml");
 
     @Test
-    public void testRoslynatorAnalyze() {
+    public void testRoslynatorLocAnalyze() {
 
         if (ROSLYN_OUTPUT.toFile().exists()) {
             try { FileUtils.forceDelete(ROSLYN_OUTPUT.toFile()); }
             catch (IOException e) { e.printStackTrace(); }
         }
 
-        RoslynatorAnalyzer analyzer = new RoslynatorAnalyzer(ROSLYN_ROOT, MS_BUILD);
+        RoslynatorLoc analyzer = new RoslynatorLoc(ROSLYN_ROOT, MS_BUILD);
         Path analyzeOutPath = analyzer.analyze(TARGET);
 
         Assert.assertTrue(analyzeOutPath.toFile().exists());
     }
 
     @Test
-    public void testRoslynatorParseAnalysis() {
+    public void testRoslynatorLocParseAnalysis() {
 
         if (ROSLYN_OUTPUT.toFile().exists()) {
             try { FileUtils.forceDelete(ROSLYN_OUTPUT.toFile()); }
             catch (IOException e) { e.printStackTrace(); }
         }
 
-        RoslynatorAnalyzer analyzer = new RoslynatorAnalyzer(ROSLYN_ROOT, MS_BUILD);
+        RoslynatorLoc analyzer = new RoslynatorLoc(ROSLYN_ROOT, MS_BUILD);
         Path analyzeOutPath = analyzer.analyze(TARGET);
 
         Assert.assertTrue(analyzeOutPath.toFile().exists());
 
         Map<String, Diagnostic> parsedAnalysis = analyzer.parseAnalysis(analyzeOutPath);
 
-        Assert.assertEquals(3, parsedAnalysis.size());
+        Assert.assertEquals(1, parsedAnalysis.size());
 
-        Assert.assertTrue(parsedAnalysis.containsKey("RCS1163"));
-        Assert.assertTrue(parsedAnalysis.containsKey("RCS1018"));
-        Assert.assertTrue(parsedAnalysis.containsKey("SCS0005"));
-
-        Assert.assertEquals(1, parsedAnalysis.get("RCS1163").getNumChildren());
-        Assert.assertEquals(1, parsedAnalysis.get("RCS1018").getNumChildren());
-        Assert.assertEquals(1, parsedAnalysis.get("SCS0005").getNumChildren());
+        Assert.assertTrue(parsedAnalysis.containsKey("loc"));
+        Assert.assertEquals(1, parsedAnalysis.get("loc").getNumChildren());
+        Assert.assertTrue(parsedAnalysis.get("loc").getChildren().values().stream()
+                .allMatch(findingNode -> findingNode.getValue() == 39));
     }
 
 }
